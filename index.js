dbc601690582f8467a79e1ce3b04588fd89027f2
Ethan Rich <ethanc.rich@gmail.com>
2022-07-07 23:29:07 -0700

    Change the three basic slash commands to be written from individual files

--- index.js
+++ index.js
@@ -5,11 +5,26 @@
  */
 
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Gather the commands from the commands Collection
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -17,16 +32,21 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
+	// If isn't a command, quit
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	// Get the command from the list by matching the name
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	// If there is no matching name, then return anyway
+	if (!command) return;
+
+	// Attempt to execute the command
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
